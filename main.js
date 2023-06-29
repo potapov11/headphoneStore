1786a4dfe9778e96b8ff86c0b1fdc678dff9715a
Serezha <volos11223344@yandex.ru>
2023-06-29 22:10:45 +0300

    added num to span succes

--- main.js
+++ main.js
@@ -1,20 +1,2 @@
-// import { createCards } from "./modules/create_cards.js";
-
-// createCards();
-
-// const btns = document.querySelectorAll(".card__btn");
-// console.log(btns);
-
-// btns.forEach((item) => {
-// 	item.addEventListener("click", () => {
-// 		console.log("click");
-// 	});
-// });
-
 import { toLocal } from "./modules/to_local_storage.js";
-// import { fromLocal } from "./modules/from_local_storage.js";
-// import { createCatalogCards } from "./modules/create_catalog_cards.js";
 toLocal();
-// const fromLocalStorageArr = fromLocal();
-// console.log(fromLocalStorageArr);
-// createCatalogCards();
--- modules/to_local_storage.js
+++ modules/to_local_storage.js
@@ -5,22 +5,21 @@ createCards();
 const toLocal = () => {
 	const btns = document.querySelectorAll(".card__btn");
 	const shopNum = document.querySelector(".header__shop-span");
-	console.log(btns);
-	let count = 0;
 
 	let toLocalArr = [];
-	shopNum.textContent = toLocalArr.length;
 
 	btns.forEach((item, i) => {
 		item.addEventListener("click", () => {
-			// count++;
 			shopNum.classList.remove("none");
-			// shopNum.textContent = count;
 			toLocalArr.push(i);
 			console.log(toLocalArr.length);
 			console.log(toLocalArr);
 			localStorage.setItem("toLocalArr", toLocalArr);
-			shopNum.textContent = toLocalArr.length;
+			// shopNum.textContent = toLocalArr.length;
+			console.log(localStorage.getItem("toLocalArr").length);
+			let newArr = Array.from(localStorage.getItem("toLocalArr"));
+			newArr = newArr.filter((item) => item !== ",");
+			shopNum.textContent = newArr.length;
 		});
 	});
 };
